c053ccb01920545073d4852317ff6808e37f1cc6
krishna <krissh.m@gmail.com>
2018-01-14 17:42:42 +0530

    spring-boot-security-oauth-basic

--- spring/spring-boot-oauth2/src/main/resources/startup.sql
+++ spring/spring-boot-oauth2/src/main/resources/startup.sql
@@ -8,3 +8,10 @@ primary key(id)
 );
 
 
+INSERT INTO user (id, username, password, salary, age) VALUES (1, 'test1', '$2a$04$b3416zAJnLpmRH5A2AKXjeOP6CY04eoMZV7n/4wb472IKMec/ct6e', 3456, 33);
+INSERT INTO user (id, username, password, salary, age) VALUES (2, 'test2', '$2a$04$7e9cUabgAcVskcJXoj8TCuuqw6wkDhFq5n5nZjrm7HWxiu4uOBkjW', 7823, 23);
+
+
+
+
+
