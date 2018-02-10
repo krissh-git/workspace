1fafd55f526db067eecffe9472016a93f859cf7a
krishna <krissh.m@gmail.com>
2018-02-10 10:08:07 +0530

    jwt

---
+++ spring/spring-security-jwt-auth/src/main/java/com/example/demo/SpringSecurityJwtAuthApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtAuthApplication.class, args);
+	}
+}
